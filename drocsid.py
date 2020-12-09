70231c4ab5611982bec7dab01807bcfd712c5bda
Aditya-A-garwal <aditya.agarwal030504@gmail.com>
2020-12-09 16:14:02 +0530

    Added boiler-plate code, files for project, running drocsid creates Contacts.txt

---
+++ drocsid.py
@@ -0,0 +1,10 @@
+import tkinter as tkt
+import sys, os, socket
+
+print("-------------------------Starting App-------------------------")
+
+open('Contacts.txt', 'w') if(not os.path.exists('Contacts.txt')) else None
+contacts_file = open('Contacts.txt', 'r+')
+
+print("-------------------------Closing App-------------------------")
+contacts_file.close()
