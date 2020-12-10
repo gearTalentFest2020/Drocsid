e58131767bc982a79e96722e3b08a9409676239f
Aditya-A-garwal <aditya.agarwal030504@gmail.com>
2020-12-10 19:27:53 +0530

    Aditya: Implemented serialization and de-serialization of contacts

--- drocsid.py
+++ drocsid.py
@@ -1,13 +1,36 @@
 import tkinter as tkt
 import sys, os, socket
 
+contacts = {}
+
+# Function to add a contact
+def addContact( name, IP):
+    pass
+# Function to remove a contact
+def remContact( IP ):
+    pass
+# Function to alter a contact
+def altContact( name, newName, IP ):
+    pass
+# Function to save all contacts
+def altContact( ):
+    pass
+
 print("-------------------------Starting App-------------------------")
 
 open('Contacts.txt', 'w') if(not os.path.exists('Contacts.txt')) else None
-contacts_file = open('Contacts.txt', 'r+')
+contacts_file = open('Contacts.txt', 'r')
+
+# Saveing contacts
+for line in contacts_file.readlines():
+    tokens = line.split()
+    assert len(tokens)==2, "Corrupted Contacts File"
+    contacts.setdefault(tokens[0], tokens[1])
+contacts_file.close()
+
+contacts_file = open('Contacts.txt', 'w')
+for IP in contacts:
+    contacts_file.write(IP + " " + contacts[IP] + '\n')
 
 print("-------------------------Closing App-------------------------")
 contacts_file.close()
-#Aditya Chowdhary was not here
-#Vyoman was here.
-#Aryan had been having here.
