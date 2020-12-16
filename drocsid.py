a46ac27800bf525b25080c415a90dfed3308eee8
ChowPow123 <adityac2504@gmail.com>
2020-12-16 15:54:10 +0530

    Chow: Fixed Frames, Added exit button.

--- drocsid.py
+++ drocsid.py
@@ -28,6 +28,15 @@ def getButton( text, command = None, h = 1, w = 48, fontSize = 20):
 # Function to add a contact
 def addContact( ):
 
+    def Close_Add():
+        contacts.addContact(ID.get(), name.get())
+        addContactFrame.destroy()
+        remContactBtn["state"] = tk.NORMAL
+        altContactBtn["state"] = tk.NORMAL
+
+    remContactBtn["state"] = tk.DISABLED
+    altContactBtn["state"] = tk.DISABLED
+
     addContactFrame = tk.Frame( master = mainWindow )
 
     name = tk.Entry( master = addContactFrame)
@@ -36,31 +45,49 @@ def addContact( ):
     ID = tk.Entry( master = addContactFrame)
     ID.insert(string = "Enter contact's ID", index = 0)
 
-    confirmBtn = tk.Button( master = addContactFrame, text = "Submit", font = ("Calibri", 12), command = lambda : contacts.addContact( ID.get(), name.get()) or addContactFrame.destroy() )
+    confirmBtn = tk.Button( master = addContactFrame, text = "Submit", font = ("Calibri", 12), command = Close_Add)
 
     name.pack()
     ID.pack()
 
     confirmBtn.pack()
-    addContactFrame.grid( row = 0, column = 1 )
+    addContactFrame.grid( row = 2, column = 1, sticky = tk.S )
 
 # Function to remove a contact
 def remContact( ):
+
+    def Close_Rem():
+        contacts.remContact(ID.get())
+        remContactFrame.destroy()
+        addContactBtn["state"] = tk.NORMAL
+        altContactBtn["state"] = tk.NORMAL
+
+    addContactBtn["state"] = tk.DISABLED
+    altContactBtn["state"] = tk.DISABLED
 
     remContactFrame = tk.Frame( master = mainWindow )
 
     ID = tk.Entry( master = remContactFrame)
     ID.insert(string = "Enter contact's ID", index = 0)
 
-    confirmBtn = tk.Button( master = remContactFrame, text = "Remove", font = ("Calibri", 12), command = lambda : contacts.remContact(ID.get()) or remContactFrame.destroy() )
+    confirmBtn = tk.Button( master = remContactFrame, text = "Remove", font = ("Calibri", 12), command = Close_Rem)
 
     ID.pack()
     confirmBtn.pack()
 
-    remContactFrame.grid( row = 0, column = 1 )
+    remContactFrame.grid( row = 2, column = 1, sticky = tk.S  )
 
 # Function to alter a contact
 def altContact( ):
+
+    def Close_Alt():
+        contacts.altContact(ID.get(), name.get())
+        altContactFrame.destroy()
+        addContactBtn["state"] = tk.NORMAL
+        remContactBtn["state"] = tk.NORMAL
+
+    addContactBtn["state"] = tk.DISABLED
+    remContactBtn["state"] = tk.DISABLED
 
     altContactFrame = tk.Frame( master = mainWindow )
 
@@ -70,7 +97,7 @@ def altContact( ):
     name = tk.Entry( master = altContactFrame )
 
     confirmBtn1 = tk.Button( master = altContactFrame, text = "Edit", font = ("Calibri", 12), command = lambda : name.insert(string = contacts[ID.get().strip()], index = 0))
-    confirmBtn2 = tk.Button( master = altContactFrame, text = "Submit", font = ("Calibri", 12), command = lambda : contacts.altContact( ID.get(), name.get() ) or altContactFrame.destroy())
+    confirmBtn2 = tk.Button( master = altContactFrame, text = "Submit", font = ("Calibri", 12), command = Close_Alt)
 
     ID.pack()
     name.pack()
@@ -78,7 +105,7 @@ def altContact( ):
     confirmBtn1.pack()
     confirmBtn2.pack()
 
-    altContactFrame.grid( row = 0, column = 1 )
+    altContactFrame.grid( row = 2, column = 1 , sticky = tk.S )
 
 def createChatroom( name ):
 
@@ -118,7 +145,8 @@ print("-------------------------Starting App-------------------------")
 UID = contacts.loadAll( )
 
 # Creating window
-mainWindow = getWindow( "Drocsid", "800x450" )
+mainWindow = getWindow("Drocsid","800x450")
+mainWindow.attributes("-fullscreen",True)
 mainWindow.configure( bg = backgroundDefault )
 
 # This is the part of the screen where you can click to add, remove, alter contacts and chatrooms
@@ -127,22 +155,27 @@ optionsFrame = tk.Frame( mainWindow )
 # This is the frame for selecting the chatrooms
 selectChatroomFrame = tk.Frame( mainWindow )
 
+Title = tk.Label(master = mainWindow, text= "DROCSID", font = ("Calibri", 24),bg = backgroundDefault, fg = "#ffffff")
+
+
 addContactBtn = tk.Button( master = optionsFrame, text = "Add Contact", font = ("Calibri", 16), command = addContact, bg = btnDefault, fg = btnTxtDefault )
 remContactBtn = tk.Button( master = optionsFrame, text = "Remove Contact", font = ("Calibri", 16), command = remContact, bg = btnDefault, fg = btnTxtDefault )
 altContactBtn = tk.Button( master = optionsFrame, text = "Edit Contact", font = ("Calibri", 16), command = altContact, bg = btnDefault, fg = btnTxtDefault )
 selRoomBtn = tk.Button( master = optionsFrame, text = "Chats", font = ("Calibri", 16), command = selectChatroom, bg = btnDefault, fg = btnTxtDefault )
+exitBtn = tk.Button( master = optionsFrame, text = "Exit", font = ("Calibri", 12), command = mainWindow.destroy, bg = "#ff0000", fg = btnTxtDefault )
 
 addContactBtn.config( height = 2, width = 20 )
 remContactBtn.config( height = 2, width = 20 )
 altContactBtn.config( height = 2, width = 20 )
 selRoomBtn.config( height = 2, width = 20 )
 
+Title.grid(row = 0,column = 0)
 addContactBtn.pack()
 remContactBtn.pack()
 altContactBtn.pack()
 selRoomBtn.pack()
-
-optionsFrame.grid( row = 0, column = 0 )
+exitBtn.pack(fill = tk.X)
+optionsFrame.grid( row = 1, column = 0 )
 
 mainWindow.mainloop()
 
