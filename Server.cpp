3662feda0668b172180e6188270f2e46711f2fff
Aditya-A-garwal <aditya.agarwal030504@gmail.com>
2021-03-07 17:49:14 +0530

    Misc

--- Server.cpp
+++ Server.cpp
@@ -1,50 +1,47 @@
-// Client side C/C++ program to demonstrate Socket programming
+#include <bits/stdc++.h>
 // #include <stdio.h>
 // #include <iostream>
-// #include <sys/socket.h>
-// #include <arpa/inet.h>
-// #include <unistd.h>
 // #include <string.h>
 
-#include <bits/stdc++.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
-#include <unistd.h>
 
 #define PORT 8080
 
 int main(int argc, char const *argv[])
 {
-	int sock = socket( AF_INET, SOCK_STREAM, 0 );
-	if(sock < 0) { std::cout << "Creation failed" << std::endl; return 0; }
+	int sock;
+	char s[1024];
 
 	sockaddr_in serv_addr;
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_port = htons(PORT);
 
-	// Convert IPv4 and IPv6 addresses from text to binary form
+	while( ( sock = socket( AF_INET, SOCK_STREAM, 0 ) ) < 0) continue;
+	printf("Successfully created socket\n");
+
 	if (inet_pton( AF_INET, "127.0.0.1", &serv_addr.sin_addr ) <= 0)
-	{ std::cout << "\nInvalid address/ Address not supported" << std::endl; return 0; }
+	{ printf("Invalid address/ Address not supported\n"); return 0; }
 
-	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-	{ std::cout << "\nConnection Failed" << std::endl; return -1; }
+	while( !connect( sock, (sockaddr *)&serv_addr, sizeof( serv_addr ) ) ) continue;
+	printf("Succesfully connected to server\n");
 
-	std::string s = "";
 	char buff[1024] = {0};
 	while(1)
 	{
-		std::cout << "SENDING: ";
-		std::cin >> s;
+		printf("SENDING: ");
+		fgets(s, 1024, stdin);
 
-		if(s.size() == 1 && s[0] == 'q') break;
+		if(strlen(s) == 2 && s[0] == 'q') break;
 
-		send( sock, s.c_str(), s.size(), 0 );
-		int numRead = read( sock, buff, 1024 );
+		send( sock, s, strlen(s) - 1, 0 );
+		int numRead = recv( sock, buff, 1024, 0 );
 
-		std::cout << "RECEIVING ";
-		for(int i = 0; i < numRead;) std::cout << buff[i++];
-		std::cout << std::endl;
+		std::cout << "RECEIVED: ";
+		printf("%s\n", buff);
 	}
+
+	shutdown( sock, SHUT_RDWR );
 
 	return 0;
 }
